b1c31d8d8aff611912a68ca6b7d6e9fc66542e2c
olioni <o.taumoepeau@taiwananga.co.nz>
2022-01-18 13:47:47 +1300

    added mathjs library to allow ez math

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
+const math = require('mathjs')
 
-const token = "YOUR-TOKEN"
+const token = "OTMyNTAxMjgxNDE3ODg3ODE1.YeT5fg.KfoZ-R4-X340GX3Jk1j62b3HNWU"
 
 const client = new Discord.Client({
 	intents: [
@@ -20,8 +21,13 @@ function checkForCmd(msg) {
 }
 
 client.on('messageCreate', (message) => {
-	if (message.content === '/test') {
-			message.reply('true')
+	let msg = message.content
+	if (msg[0] === '!') {
+		if (msg.startsWith('!calc')) {
+			let eq = msg.split(' ')[1]
+			let answer = math.evaluate(eq).toString()
+			message.reply(answer)
+		}
 	}
 })
 
